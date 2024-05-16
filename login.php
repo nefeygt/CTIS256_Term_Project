3927a190cbd7c3f4daf28be9be8f3e34ca8f10c5
Barış Cihanoğlu <bariscihanoglu4@gmail.com>
2024-05-16 22:30:41 +0300

    fix session check failure

--- login.php
+++ login.php
@@ -2,6 +2,20 @@
 session_start();
 require "db.php";
 
+if(isset($_SESSION['token'])) {
+    // Assuming you have a function to verify and get user data by token
+    $user = getCustomerByToken($_SESSION['token']); // Replace with your actual function
+    if ($user != false) {
+        header("Location: customer");
+        exit;
+    }
+    $user = getMarketByToken($_SESSION['token']);
+    if ($user != false) {
+        header("Location: market");
+        exit;
+    }
+}
+
 if (isset($_POST['login'])) {
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $password = $_POST['password'];
