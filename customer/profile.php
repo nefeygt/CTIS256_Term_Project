67184cbc853d40a5a0e82727674d6ff7a41312d1
Barış Cihanoğlu <bariscihanoglu4@gmail.com>
2024-05-20 17:34:00 +0300

    error handling

--- customer/profile.php
+++ customer/profile.php
@@ -9,21 +9,20 @@ if (!isset($_SESSION['token'])) {
     exit;
 }
 
-// Fetch the market information using the email stored in the session
+// Fetch the customer information using the email stored in the session
 $token = $_SESSION['token'];
-$marketInfo = getCustomerByToken($token);
-// If the market information is not found, display an error message and exit
-if ($marketInfo == false) {
-    echo "Market information not found.";
+$customerInfo = getCustomerByToken($token);
+// If the customer information is not found, display an error message and exit
+if ($customerInfo == false) {
+    echo "Customer information not found.";
     exit;
 }
 
 // Access individual fields
-$name = htmlspecialchars($marketInfo['name']);
-$address = htmlspecialchars($marketInfo['address']);
-$city = htmlspecialchars($marketInfo['city']);
-$district = htmlspecialchars($marketInfo['district']);
-// $email = htmlspecialchars($marketInfo['email']);
+$name = htmlspecialchars($customerInfo['name']);
+$address = htmlspecialchars($customerInfo['address']);
+$city = htmlspecialchars($customerInfo['city']);
+$district = htmlspecialchars($customerInfo['district']);
 ?>
 
 <!DOCTYPE html>
@@ -32,22 +31,22 @@ $district = htmlspecialchars($marketInfo['district']);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Market Profile</title>
+    <title>Customer Profile</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.tailwindcss.com" rel="stylesheet">
 </head>
 <body>
     <div class="container mx-auto px-4">
         <div class="navbar bg-gray-800 flex justify-between items-center my-4 p-4 text-white">
-            <a href="./index.php" class="text-blue-300 hover:text-blue-500"><i class="fas fa-store mr-2"></i>Market</a>
+            <a href="./index.php" class="text-blue-300 hover:text-blue-500"><i class="fas fa-store mr-2"></i>Home</a>
             <a href="./profile.php" class="text-blue-300 hover:text-blue-500 flex items-center">
                 <i class="fas fa-user-circle mr-2"></i>
                 <?= $name ?>
             </a>
         </div>
         <div class="mb-4">
-            <h2 class="text-2xl font-bold mb-2">Market Profile</h2>
-            <p><strong>Consumer Name:</strong> <?= $name ?></p>
+            <h2 class="text-2xl font-bold mb-2">Customer Profile</h2>
+            <p><strong>Customer Name:</strong> <?= $name ?></p>
             <p><strong>Address:</strong> <?= $address ?></p>
             <p><strong>City:</strong> <?= $city ?></p>
             <p><strong>District:</strong> <?= $district ?></p>
