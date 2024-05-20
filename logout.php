170e079041399b5d819f99b6ca089fd28a6cfea7
nefeygt <n.efeyigit@outlook.com>
2024-05-20 17:44:11 +0300

    logout dynamic

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header("Location: ./login.php"); // Redirect to the main registration page
+header("Location: ./"); // Redirect to the main registration page
 exit;
 ?>
